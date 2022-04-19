1ec93fce2349eaeb7e7f165306a2736c747d5e9e
Ismael <ikkotheunforgiven@gmail.com>
2022-04-19 21:22:15 +0200

    Aplicacion con Nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <section class="section">
+      <h1 class="title is-1">Fukuro Manga</h1>
+      <button class="button is-info">Enter</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
