1dbc4821adbbb122cc435d0ca1c33125872387a8
Ismael <ikkotheunforgiven@gmail.com>
2022-05-10 22:41:29 +0200

    Footer hecho

---
+++ layouts/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer class="footer mt-5 has-background-black">
+        <div class="container">
+            <div class="content">
+                <p class="has-text-left pt-3">&copy; 2022 Fukuro Manga, Inc</p>
+                <div class="social pt-3">
+                    <a href="https://twitter.com/ikkounforgiven">
+                        <img src="https://img.icons8.com/ios/24/FFFFFF/twitter--v1.png"/>
+                    </a>
+                    <a href="https://www.instagram.com/ikkotheunforgiven/">
+                        <img src="https://img.icons8.com/material-outlined/25/FFFFFF/instagram-new--v1.png"/>
+                    </a>
+                    <a href="https://www.pinterest.es/ismaele54/">
+                        <img src="https://img.icons8.com/material-outlined/24/FFFFFF/pinterest--v1.png"/>
+                    </a>
+                    <a href="https://www.facebook.com/IkkoTheUnforgiven/">
+                        <img src="https://img.icons8.com/material-outlined/24/FFFFFF/facebook-new.png"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+    .content {
+        border-top: 1px solid #dee2e6;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
