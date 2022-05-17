be6763b19f87ee8edccf7667f9cd92940d9f2adc
Ismael <ikkotheunforgiven@gmail.com>
2022-05-17 21:33:54 +0200

    Arreglando detalles

---
+++ components/Detail.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li v-if="item == 'manga'"><a :href="'/'+categoria" class="is-capitalized">{{ categoria }}</a></li>
+                <li v-else><a href="/merchandising">Merchandising</a></li>
+                <li class="is-active"><a href="#">{{ nombre }}</a></li>
+            </ul>
+        </nav>
+        <div class="columns">
+            <div class="column is-two-fifths">
+                <img :src="imagen"/>
+            </div>
+            <div class="column">
+                <h1 class="title is-1">{{ nombre }}</h1>
+                <label class="label" v-if="item == 'manga'">Autor:</label>
+                <label class="label" v-else>Fabricante:</label>
+                <span>{{ fabricante }}</span>
+                <label class="label pt-4" v-if="item == 'manga'">Género:</label>
+                <label class="label pt-4" v-else>Tipo:</label>
+                <span v-if="item == 'manga'">{{ genero }}</span>
+                <span v-else class="is-capitalized">{{ tipo }}</span>
+                <label class="label pt-4">Precio:</label>
+                <span>{{ precio }}€</span>
+                <p class="pt-6 pb-5 has-text-justified">{{ descripcion }}</p>
+                <hr>
+                <div class="buttons">
+                    <img src="https://img.icons8.com/material-sharp/24/000000/filled-like.png" class="pb-2"/>
+                    <label class="label pl-1">{{ likes }}</label>
+                    <img src="https://img.icons8.com/material-sharp/24/000000/dislike.png" class="pb-2 pl-4"/>
+                    <label class="label pl-1">{{ dislikes }}</label>
+                    <button class="button is-danger">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="tabs is-boxed">
+            <ul>
+                <li class="is-active"><a>Comentarios</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: String,
+            default: ''
+        },
+        tipo: {
+            type: String,
+            default: ''
+        },
+        categoria: {
+            type: String,
+            default: ''
+        },
+        nombre: {
+            type: String,
+            default: ''
+        },
+        imagen: {
+            type: String,
+            default: ''
+        },
+        fabricante: {
+            type: String,
+            default: ''
+        },
+        genero: {
+            type: String,
+            default: ''
+        },
+        precio: {
+            type: Number,
+            default: ''
+        },
+        descripcion: {
+            type: String,
+            default: ''
+        },
+        likes: {
+            type: Number,
+            default: ''
+        },
+        dislikes: {
+            type: Number,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        margin-left: auto;
+    }
+</style>
--- pages/_category/_name.vue
+++ pages/_category/_name.vue
@@ -1,89 +1,46 @@
 <template>
     <div class="container">
         <div class="manga" v-if="mangaActual.length != 0">
-            <nav class="breadcrumb" aria-label="breadcrumbs">
-                <ul>
-                    <li><a href="/">Inicio</a></li>
-                    <li><a :href="'/'+mangaActual.category" class="is-capitalized">{{ mangaActual.category }}</a></li>
-                    <li class="is-active"><a href="#">{{ mangaActual.name }}</a></li>
-                </ul>
-            </nav>
-            <div class="columns">
-                <div class="column is-two-fifths">
-                    <img :src="mangaActual.image"/>
-                </div>
-                <div class="column">
-                    <h1 class="title is-1">{{ mangaActual.name }}</h1>
-                    <label class="label">Autor:</label>
-                    <span>{{ mangaActual.author }}</span>
-                    <label class="label pt-4">Género:</label>
-                    <span>{{ mangaActual.genre }}</span>
-                    <label class="label pt-4">Precio:</label>
-                    <span>{{ mangaActual.price }}€</span>
-                    <p class="pt-6 pb-5 has-text-justified">{{ mangaActual.description }}</p>
-                    <hr>
-                    <div class="buttons">
-                        <img src="https://img.icons8.com/material-sharp/24/000000/filled-like.png" class="pb-2"/>
-                        <label class="label pl-1">{{ mangaActual.likes }}</label>
-                        <img src="https://img.icons8.com/material-sharp/24/000000/dislike.png" class="pb-2 pl-4"/>
-                        <label class="label pl-1">{{ mangaActual.dislikes }}</label>
-                        <button class="button is-danger">Añadir al carrito</button>
-                    </div>
-                </div>
-            </div>
-            <div class="tabs is-boxed">
-                <ul>
-                    <li class="is-active"><a>Comentarios</a></li>
-                </ul>
-            </div>
+            <Detail
+                :item="mangaActual.item"
+                :nombre="mangaActual.name"
+                :imagen="mangaActual.image"
+                :fabricante="mangaActual.author"
+                :genero="mangaActual.genre"
+                :precio="mangaActual.price"
+                :descripcion="mangaActual.description"
+                :likes="mangaActual.likes"
+                :dislikes="mangaActual.dislikes"
+                :categoria="mangaActual.category"
+            />
         </div>
         
 
         <div class="merch" v-if="articuloActual.length != 0">
-            <nav class="breadcrumb" aria-label="breadcrumbs">
-                <ul>
-                    <li><a href="/">Inicio</a></li>
-                    <li><a href="/merchandising">Merchandising</a></li>
-                    <li class="is-active"><a href="#">{{ articuloActual.name }}</a></li>
-                </ul>
-            </nav>
-            <div class="columns">
-                <div class="column is-two-fifths">
-                    <img :src="articuloActual.image"/>
-                </div>
-                <div class="column">
-                    <h1 class="title is-1">{{ articuloActual.name }}</h1>
-                    <label class="label">Fabricante:</label>
-                    <span>{{ articuloActual.manufacturer }}</span>
-                    <label class="label pt-4">Tipo:</label>
-                    <span class="is-capitalized">{{ articuloActual.type }}</span>
-                    <label class="label pt-4">Precio:</label>
-                    <span>{{ articuloActual.price }}€</span>
-                    <p class="pt-6 pb-5 has-text-justified">{{ articuloActual.description }}</p>
-                    <hr>
-                    <div class="buttons">
-                        <img src="https://img.icons8.com/material-sharp/24/000000/filled-like.png" class="pb-2"/>
-                        <label class="label pl-1">{{ articuloActual.likes }}</label>
-                        <img src="https://img.icons8.com/material-sharp/24/000000/dislike.png" class="pb-2 pl-4"/>
-                        <label class="label pl-1">{{ articuloActual.dislikes }}</label>
-                        <button class="button is-danger">Añadir al carrito</button>
-                    </div>
-                </div>
-            </div>
-            <div class="tabs is-boxed">
-                <ul>
-                    <li class="is-active"><a>Comentarios</a></li>
-                </ul>
-            </div>
+            <Detail
+                :item="articuloActual.item"
+                :nombre="articuloActual.name"
+                :imagen="articuloActual.image"
+                :fabricante="articuloActual.manufacturer"
+                :precio="articuloActual.price"
+                :descripcion="articuloActual.description"
+                :likes="articuloActual.likes"
+                :dislikes="articuloActual.dislikes"
+                :tipo="articuloActual.type"
+            />
         </div>
     </div>
 </template>
 
 <script>
+import Detail from "~/components/Detail"
 import { db } from "~/plugins/firebase"
 import { collection, getDocs } from 'firebase/firestore'
 
 export default {
+    components: {
+        Detail
+    },
     created() {
         this.getManga(),
         this.getItem()
@@ -112,7 +69,5 @@ export default {
 </script>
 
 <style scoped>
-    button {
-        margin-left: auto;
-    }
+    
 </style>
