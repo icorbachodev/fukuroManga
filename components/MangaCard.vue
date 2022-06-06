d8cc4a9106a06fdd31863f9baad42dc3cdfbd886
Ismael <ikkotheunforgiven@gmail.com>
2022-06-06 16:04:33 +0200

    Arreglando rutas

--- components/MangaCard.vue
+++ components/MangaCard.vue
@@ -3,7 +3,7 @@
         <div class="card has-text-centered">
             <div class="card-image">
                 <figure class="image">
-                    <a :href="categoria + '/' + id"><img :src="imagen" alt="Imagen manga"/></a>
+                    <a :href="'/'+ categoria + '/' + id"><img :src="imagen" alt="Imagen manga"/></a>
                 </figure>
             </div>
             <div class="card-content">
