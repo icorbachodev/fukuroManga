63917fa42e085401e21cf77a216c354b4f084645
Ismael <ikkotheunforgiven@gmail.com>
2022-06-07 10:46:34 +0200

    Protegiendo panel de admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ import { auth } from "~/plugins/firebase"
 
 export default function({ redirect }) {
     auth.onAuthStateChanged(user => {
-        if(!user) {
+        if(!user || user.email != 'admin@gmail.com') {
             return redirect('/login')
         }
     })
